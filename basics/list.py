53182fba8c6044851908490721526ada7d87859e
JoseOgam <jotienoochieng@gmail.com>
2023-02-20 14:33:16 +0300

    python basics

---
+++ basics/list.py
@@ -0,0 +1,14 @@
+mylist = []
+mylist.append(0)
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+mylist.append(4)
+mylist.append(5)
+mylist.append(6)
+mylist.append(7)
+mylist.append(8)
+mylist.append(9)
+
+for x in mylist:
+    print(x)
